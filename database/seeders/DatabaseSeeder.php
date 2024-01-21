f3c3535996ab44397a59b7082a4021b5a6fcd2d8
Ruslan Kuchiev <kuchiev.ruslan@stmd.pro>
2024-01-21 19:37:09 +0500

    add admin middleware

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory()->create([
+             'name' => 'Ekaterina',
+             'email' => 'e.starceva@betterlife.ru',
+             'password' => '123456',
+             'role' => 'admin'
+         ]);
     }
 }
