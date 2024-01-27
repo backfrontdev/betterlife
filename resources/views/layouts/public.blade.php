5131e96aef68e21e0ab617eb7a1bb76a41d1a883
Ruslan Kuchiev <kuchiev.ruslan@stmd.pro>
2024-01-27 20:33:24 +0500

    footer

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -8,16 +8,17 @@
     @vite(['resources/css/app.css'])
 </head>
 <body class="bg-slate-100">
-    <header class="border-b">
-        <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-            <div class="flex lg:flex-1">
-                <a href="/" class="-m-1.5 p-1.5 text-xl font-bold sm:text-4xl">
-                    Better Life
-                </a>
-            </div>
-            @if(false)
+<header class="border-b">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div class="flex lg:flex-1">
+            <a href="/" class="-m-1.5 p-1.5 text-xl font-bold sm:text-4xl">
+                Better Life
+            </a>
+        </div>
+        @if(false)
             <div class="flex lg:hidden">
-                <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+                <button type="button"
+                        class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
                     <span class="sr-only">Открыть основное меню</span>
                     <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                          aria-hidden="true">
@@ -26,14 +27,14 @@
                     </svg>
                 </button>
             </div>
-            @endif
-            <div class="hidden lg:flex lg:gap-x-12">
-                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Сертификаты</a>
-                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Услуги</a>
-                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Обо мне</a>
-            </div>
-        </nav>
-        @if(false)
+        @endif
+        <div class="hidden lg:flex lg:gap-x-12">
+            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Сертификаты</a>
+            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Услуги</a>
+            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Обо мне</a>
+        </div>
+    </nav>
+    @if(false)
         <div class="lg:hidden" role="dialog" aria-modal="true">
             <div class="fixed inset-0 z-10"></div>
             <div
@@ -64,7 +65,7 @@
                                 Услуги
                             </a>
                             <a href="{{ @route('index') }}"
-                                   class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                               class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                             >
                                 Обо мне
                             </a>
@@ -73,10 +74,29 @@
                 </div>
             </div>
         </div>
-        @endif
-    </header>
-    <div class="max-w-7xl mx-auto p-6 lg:px-8">
-        {{ $slot }}
+    @endif
+</header>
+<div class="max-w-7xl mx-auto p-6 lg:px-8">
+    {{ $slot }}
+</div>
+<footer class="bg-transparent shadow border-t">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span class="text-sm text-gray-500 sm:text-center">© {{ date('Y') }} <a href="/" class="hover:underline">Better Life</a>. Все права защищены.</span>
+        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
+            <li>
+                <a href="#" class="hover:underline me-4 md:me-6">Сертификаты</a>
+            </li>
+            <li>
+                <a href="#" class="hover:underline me-4 md:me-6">Услуги</a>
+            </li>
+            <li>
+                <a href="#" class="hover:underline me-4 md:me-6">Обо мне</a>
+            </li>
+            <li>
+                <a href="https://katrinllandtg.t.me/" class="hover:underline" target="_blank">Telegram</a>
+            </li>
+        </ul>
     </div>
+</footer>
 </body>
 </html>
